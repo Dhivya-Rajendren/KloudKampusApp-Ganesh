5374400767f47a2fb948be198ad76f13edb7567c
Dhivya-Rajendren <rajendren.dhivya@gmail.com>
2023-02-07 19:43:01 +0530

    ADO .NET Code

--- KloudKampusApp/Controllers/TrainersController.cs
+++ KloudKampusApp/Controllers/TrainersController.cs
@@ -10,45 +10,8 @@ namespace KloudKampusApp.Controllers
 
         public IActionResult Index()
         {
-            Trainer dhivya = new Trainer();//Creating an object 
-            dhivya.TrainerName = "Dhivya Rajendren";
-            dhivya.TrainerId = 1;
-            dhivya.Skills = "Sql, Angular,EF Core, ASP .NET Core";
-            dhivya.YearsOfExperience = 10;
-            dhivya.TrainerImg = "/images/dhivya.png";
-
-            trainers.Add(dhivya);
-
-            Trainer aabha = new Trainer();
-            aabha.TrainerName = "Aabha Bhusan";
-            aabha.TrainerId = 2;
-            aabha.Skills = "Java,AWS";
-            aabha.YearsOfExperience = 12;
-            aabha.TrainerImg = "/images/Aabha.png";
-
-            trainers.Add(aabha);
-
-            //string name = "Dhivya";
-            ////Pass the data from a controller to view 
-            //// ViewBag
-            ////ViewData
-            ////TempData
-            ////Models
-
-
-            //List<string> names = new List<string>();
-            //names.Add("Dhivya");
-            //names.Add("Aabha");
-            //names.Add("geetha");
-
-            //ViewBag.Names = names;
-
-            //ViewData["Name"] = name;
-
-            //ViewData["Names"] = names;
-            //TempData["Names"] = names;
-
-            //ViewData["Trainers"] = trainers;
+            DBHelper db = new DBHelper();
+            trainers = db.GetTrainers();
 
 
             return View(trainers);
@@ -79,19 +42,17 @@ namespace KloudKampusApp.Controllers
         [HttpPost]
         public IActionResult AddNewTrainer(Trainer trainer)
         {
-            if (ModelState.IsValid)
-            {
-                trainers.Add(trainer);
-            }
+            DBHelper db = new DBHelper();
+            db.AddNewTrainer(trainer);
+
            
             return RedirectToAction("Index");
         }
 
         public IActionResult DeleteTrainer(int trainerId)
         {
-            Trainer trainer = trainers.Find(t => t.TrainerId == trainerId);
-            trainers.Remove(trainer);
-
+            DBHelper db = new DBHelper();
+            db.DeleteTrainer(trainerId);
             return RedirectToAction("Index");
 
         }
---
+++ KloudKampusApp/Models/DBHelper.cs
@@ -0,0 +1,63 @@
+﻿using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace KloudKampusApp.Models
+{
+    public class DBHelper
+    {
+
+        // ADO .Net Code
+        SqlConnection con;
+        SqlCommand com;
+        SqlDataReader reader;
+
+        public List<Trainer> GetTrainers()
+        {
+            List<Trainer> trainers = new List<Trainer>();
+
+            string conString = "Server=(localdb)\\MSSQLLocaldb;Database=KloudKampusDB;integrated security=true";
+            con = new SqlConnection(conString);
+            con.Open();
+            com = new SqlCommand("Select * from tbl_Trainers", con);
+            reader = com.ExecuteReader();
+            while(reader.Read())
+            {
+                Trainer trainer = new Trainer();
+                trainer.TrainerId = reader.GetInt32(0);//First column
+                trainer.TrainerName = reader.GetString(1);
+                trainer.YearsOfExperience = reader.GetInt32(2);
+                trainer.Skills = reader.GetString(3);
+                trainer.TrainerImg = reader.GetString(4);
+
+                trainers.Add(trainer);
+            }
+            reader.Close();
+            con.Close();
+
+            return trainers;
+
+        }
+
+        public void AddNewTrainer(Trainer trainer)
+        {
+            string conString = "Server=(localdb)\\MSSQLLocaldb;Database=KloudKampusDB;integrated security=true";
+            con = new SqlConnection(conString);
+            con.Open();
+            com = new SqlCommand("insert into tbl_trainers values(" + trainer.TrainerId + ",'" + trainer.TrainerName + "'," + trainer.YearsOfExperience + ",'" + trainer.Skills + "','" + trainer.TrainerImg + "')", con);
+            com.ExecuteNonQuery();
+        }
+
+        public void DeleteTrainer(int trainerId)
+        {
+            string conString = "Server=(localdb)\\MSSQLLocaldb;Database=KloudKampusDB;integrated security=true";
+            con = new SqlConnection(conString);
+            con.Open();
+            com = new SqlCommand("Delete from tbl_trainers where trainerid=" + trainerId, con);
+            com.ExecuteNonQuery();
+        }
+    }
+}
+
+
+
+
